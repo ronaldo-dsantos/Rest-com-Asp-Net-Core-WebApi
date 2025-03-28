2f302585a6f8129b4d8930e76bf82ff17a286f28
ronaldo <ronaldo.dsantos@gmail.com>
2025-03-27 23:18:41 -0300

    add concluindo a modelagem da api

--- src/DevIO.Api/Program.cs
+++ src/DevIO.Api/Program.cs
@@ -1,36 +1,10 @@
 using DevIO.Api.Configuration;
 using DevIO.Data.Context;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.SuppressModelStateInvalidFilter = true;
-});
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("Development",
-        builder =>
-            builder
-            .AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader());
-
-
-    //options.AddPolicy("Production",
-    //    builder =>
-    //        builder
-    //            .WithMethods("GET")
-    //            .WithOrigins("http://desenvolvedor.io")
-    //            .SetIsOriginAllowedToAllowWildcardSubdomains()
-    //            //.WithHeaders(HeaderNames.ContentType, "x-custom-header")
-    //            .AllowAnyHeader());
-});
 
 builder.Services.AddDbContext<MeuDbContext>(options =>
 {
@@ -39,21 +13,9 @@ builder.Services.AddDbContext<MeuDbContext>(options =>
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-builder.Services.AddOpenApi();
-
 builder.Services.ResolveDependencies();
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseCors("Development");
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
