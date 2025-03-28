89995e7c68db56675c6fd13198c77121d7a922f3
ronaldo <ronaldo.dsantos@gmail.com>
2025-03-28 13:45:51 -0300

    add customizando as mensagens de erros do identity

--- src/DevIO.Api/Configuration/IdentityConfig.cs
+++ src/DevIO.Api/Configuration/IdentityConfig.cs
@@ -1,4 +1,5 @@
 ﻿using DevIO.Api.Data;
+using DevIO.Api.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,7 +15,7 @@ namespace DevIO.Api.Configuration
             services.AddDefaultIdentity<IdentityUser>()
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
-                //.AddErrorDescriber<IdentityMensagensPortugues>()
+                .AddErrorDescriber<IdentityMensagensPortugues>()
                 .AddDefaultTokenProviders();
 
             return services;
