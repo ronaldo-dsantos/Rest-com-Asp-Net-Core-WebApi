f0df286793c268b3ce8d41125e151b4df7b4b5f5
ronaldo <ronaldo.dsantos@gmail.com>
2025-03-29 15:44:33 -0300

    add swagger

--- src/DevIO.Api/Program.cs
+++ src/DevIO.Api/Program.cs
@@ -1,5 +1,6 @@
 using DevIO.Api.Configuration;
 using DevIO.Data.Context;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,10 +16,16 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddApiConfig();
 
+builder.Services.AddSwaggerConfig();
+
 builder.Services.ResolveDependencies();
 
 var app = builder.Build();
 
+var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
 app.UseApiConfig(app.Environment);
+
+app.UseSwaggerConfig(apiVersionDescriptionProvider);
 
 app.Run();
