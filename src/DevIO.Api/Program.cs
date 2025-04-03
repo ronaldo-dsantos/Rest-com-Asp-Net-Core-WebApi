33ec5bc81030331f6e772ea3cafc5ca250abfe5c
ronaldo <ronaldo.dsantos@gmail.com>
2025-04-03 00:45:46 -0300

    add realizando o deploy no azure

--- src/DevIO.Api/Program.cs
+++ src/DevIO.Api/Program.cs
@@ -5,6 +5,12 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
 builder.Services.AddDbContext<MeuDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
